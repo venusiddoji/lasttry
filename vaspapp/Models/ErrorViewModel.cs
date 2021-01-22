cae75a23dbe124ee2e8abab0e12901ac017d074e
venusiddoji <75560999+venusiddoji@users.noreply.github.com>
2021-01-22 21:51:18 +0000

    first commit

---
+++ vaspapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace vaspapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
